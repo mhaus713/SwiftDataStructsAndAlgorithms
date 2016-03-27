48b619a8f866941dbf7f588e99a7e0e31420c6ba
mhaus713 <mhaus713@gmail.com>
2016-03-26 18:51:41 -0600

    first commit

---
+++ Data Structs and Algorithms/Stack.swift
@@ -0,0 +1,9 @@
+//
+//  Stack.swift
+//  Data Structs and Algorithms
+//
+//  Created by Michael Hollingshaus on 3/20/16.
+//  Copyright © 2016 LittleGiant. All rights reserved.
+//
+
+import Foundation
